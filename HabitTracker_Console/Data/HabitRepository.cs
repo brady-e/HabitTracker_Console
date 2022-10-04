c5bc7d730345f2d346afbfa984228fb463f4d594
brady <brady.e96@gmail.com>
2022-10-04 07:22:00 -0600

    Creating SQLite  commands/methods. InsertHabit in process.

---
+++ HabitTracker_Console/Data/HabitRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Data.Sqlite;
+
+
+namespace HabitTracker_Console.Data
+{
+    internal class HabitRepository
+    {
+        static readonly string connectionString = @"Data Source=habit-Tracker.db";
+        void CreateDatabase()
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+
+                using (var tableCmd = connection.CreateCommand())
+                {
+                    connection.Open();
+                    tableCmd.CommandText =
+                        @"CREATE TABLE IF NOT EXISTS yourHabit (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Date TEXT,
+                        Quantity INTEGER
+                        )";
+
+                    tableCmd.ExecuteNonQuery();
+                }
+
+            }
+        }
+
+        void InsertHabit(DateTime dateTime, int quantity)
+        {
+            var query = "INSERT INTO yourHabit(Date, Quantity)" +
+                "VALUES (@dateTime, @quantity)";
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                using (var insertCmd = connection.CreateCommand(connectionString))
+                {
+                    // Define command parameters.
+                    
+                }
+            }
+        }
+
+    }
+}
